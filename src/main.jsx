15090242460eba6311c63ba27d06143aef8f0bb6
jensuki <jeni.suk@proton.me>
2025-01-17 00:43:13 -0800

    reenabled strict mode for deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,26 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import { BrowserRouter } from 'react-router-dom';
-// import './styles/index.css';
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </StrictMode>,
-// )
-
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
-import App from './App.jsx';
+import App from './App.jsx'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
 
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter } from 'react-router-dom';
+// import './styles/index.css';
+// import App from './App.jsx';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// root.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// );
